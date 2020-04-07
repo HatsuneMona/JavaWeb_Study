6658961f06772d64f93059e6163fa5a2c6893f6b
Hatsune Mona <songmiao39@foxmail.com>
2020-04-07 21:30:30 +0800

    第五次重构，饶了我吧

---
+++ src/main/webapp/resources/js/mainjs.js
@@ -0,0 +1,15 @@
+function showshebao() {
+  document.getElementById("main_iframe").src = "./Calculation.jsp";
+}
+
+function showmain() {
+  document.getElementById("main_iframe").src = "https://www.moeneko.top";
+}
+
+function showlogin() {
+  document.getElementById("main_iframe").src = "./login.jsp";
+}
+
+function showReg() {
+  document.getElementById("main_iframe").src = "./StudentReg.jsp";
+}
