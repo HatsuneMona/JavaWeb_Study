17ea2e15d6ab7226002f20d7890c325ac29cb58c
Hatsune Mona <songmiao39@foxmail.com>
2020-04-11 15:57:42 +0800

    添加所有Model的DAO类

---
+++ src/main/java/DAO/DepartmentDao.java
@@ -0,0 +1,51 @@
+package DAO;/**
+ * Created by Hatsune Mona on 2020/4/10. 初音萌奈什喵的最可爱了喵！
+ */
+
+import DBUtil.SQLConnect;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import model.DepartmentsinfoEntity;
+import model.StudentsinfoEntity;
+
+/**
+ * @program: JavaWeb
+ * @description: 对courseinfo数据库的操作
+ * @author: Created by Hatsune Mona. 初音萌奈什喵的最可爱了喵！
+ * @create: 2020-04-10 21:12
+ **/
+public class DepartmentDao {
+  
+  static public int InsertCourse(DepartmentsinfoEntity deptInfo) {
+    Connection connection = SQLConnect.getConnection();//打开数据库连接
+    PreparedStatement pstmt = null;
+    String insertSQL = "INSERT INTO departmentsinfo VALUES(?,?,?)";
+    int flag = 0;
+    try {
+      pstmt = connection.prepareStatement(insertSQL);
+      pstmt.setInt(1, deptInfo.getDepartmentNo());
+      pstmt.setString(2, deptInfo.getDepartmentName());
+      pstmt.setInt(3, deptInfo.getDepartmentHead());
+      flag = pstmt.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      if (pstmt != null) {
+        try {
+          pstmt.close();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+      if (connection != null) {
+        try {
+          connection.close();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    return flag;
+  }
+}
