5943b1eca9134f0eac18981be053a2f35307352f
Hatsune Mona <songmiao39@foxmail.com>
2020-04-18 21:51:35 +0800

    删除多余的测试类

--- src/test/java/testDAO/DepartmentDaoTest.java
+++ src/test/java/testDAO/DepartmentDaoTest.java
@@ -1,35 +1,66 @@
 package testDAO; 
 
-import org.junit.Test; 
-import org.junit.Before; 
-import org.junit.After; 
+import DAO.DepartmentDao;
+import model.DepartmentsinfoEntity;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 
-/** 
-* DepartmentDao Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>4月 11, 2020</pre> 
-* @version 1.0 
-*/ 
-public class DepartmentDaoTest { 
-
-@Before
-public void before() throws Exception { 
-} 
-
-@After
-public void after() throws Exception { 
-} 
-
-/** 
-* 
-* Method: InsertDept(DepartmentsinfoEntity deptInfo) 
-* 
-*/ 
-@Test
-public void testInsertDept() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-
+/**
+ * DepartmentDao Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>4月 18, 2020</pre>
+ */
+public class DepartmentDaoTest {
+  
+  DepartmentsinfoEntity deptinfo = new DepartmentsinfoEntity();
+  
+  
+  @Before
+  public void before() throws Exception {
+    deptinfo.setDepartmentNo(95);
+    deptinfo.setDepartmentName("DAO测试系");
+    deptinfo.setDepartmentHead(9500001);
+  }
+  
+  @After
+  public void after() throws Exception {
+    try {
+      if (DepartmentDao.SearchDept(95).size() == 1) {
+        DepartmentDao.DeleteDept(95);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  
+  /**
+   * Method: InsertDept(DepartmentsinfoEntity deptInfo)
+   */
+  @Test
+  public void testInsertDept() throws Exception {
+    DepartmentDao.InsertDept(deptinfo);
+    System.out.println(DepartmentDao.SearchDept(95).get(0).getDepartmentName());
+  }
+  
+  /**
+   * Method: SearchDept(Integer deptNo)
+   */
+  @Test
+  public void testSearchDept() throws Exception {
+    System.out.println("数据库现有系部数量：");
+    System.out.println(DepartmentDao.SearchDept(null).size());
+  }
+  
+  /**
+   * Method: DeleteDept(int deptNo)
+   */
+  @Test
+  public void testDeleteDept() throws Exception {
+    DepartmentDao.DeleteDept(95);
+  }
+  
+  
 } 
